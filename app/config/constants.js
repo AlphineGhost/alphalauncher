cae80b20e17797a3e5482ae313336812f0d72a76
AlphineGhost <alphineghostgaming@gmail.com>
2020-04-11 21:52:25 -0500

    Add files via upload

---
+++ app/config/constants.js
@@ -0,0 +1,4 @@
+const APP_DATA_NAME = 'LauncherData'
+const DISTRIBUTION_URL = 'https://dl.dropboxusercontent.com/s/xf7nyhyitw7iyi7/distribution.json?dl=0'
+
+module.exports = { APP_DATA_NAME, DISTRIBUTION_URL }
